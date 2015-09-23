b715f0a1e98520d003b33eafb4f5479bf85c39be
Felix Schlitter <felixSchlitter@gmail.com>
2015-09-24 08:41:56 +1200

    Update comment

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 import _ from 'lodash';
 
 /**
- * Solve the ordered jobs mantra
+ * Solve the ordered jobs kata.
  *
  * http://invalidcast.tumblr.com/post/52980617776/the-ordered-jobs-kata
+ * 
+ * This is naiive, unoptimized and uses recursion which will overflow the
+ * stack if the list of jobs is long enough (unless babel works some magic
+ * here, e.g. transpiles this into trampolines).
  *
- * @param {Array<Job>}
- * The jobs to resolve, where each {Job} can depend on other jobs.
- *
- * @returns {Array<Job>}
+ * @param {Array<{ name: String, depends: String }>} jobs
+ * The jobs to resolve, where each jobs can depend on other jobs.
+ * 
+ * @param {Array<String>} [solved]
+ * The list of solved jobs (used internally to recurse).
+ * 
+ * @returns {Array<String>}
+ * The list of job names in order.
  */
 export default function run(jobs=[], solved=[]) {
   return jobs.length
